79c4644438f3507248d9368db0e3369da809e110
StiflerPasha <dvoynospavel@gmail.com>
2019-12-10 21:54:18 +0300

    add api

--- server/index.js
+++ server/index.js
@@ -9,22 +9,22 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const posts = require('./routes/api/posts');
+const keys = require('./routes/api/keys');
 
 app.use('/api/posts', posts);
-
+app.use('/api/keys', keys);
 
 
 // Handle production
 if (process.env.NODE_ENV === 'production') {
-	// Static folder
-	app.use(express.static(__dirname + '/public/'));
-	
-	// Handle SPA
-	app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
+  
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
-
 
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${ port }`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
